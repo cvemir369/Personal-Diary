555bb2bb2b18ec200c14e86c36792f834c8270f4
cvemir369 <cvemir369@gmail.com>
2024-12-18 09:08:21 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="flex flex-col gap-2 items-center content-center">
+      <h1 className="custom-h1">Personal Diary</h1>
+      <p>Good Luck with the Proyect :)</p>
+    </div>
+  );
+}
+
+export default App;
