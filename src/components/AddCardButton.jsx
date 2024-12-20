741e420ff9efbdc4b00a69af0c3e55c49e90f11c
Paul Kim <ppaauullkkiimm@gmail.com>
2024-12-20 07:56:42 +0100

    added add card

--- src/components/AddCardButton.jsx
+++ src/components/AddCardButton.jsx
@@ -1,8 +1,31 @@
+import { useState } from "react";
+import { createPortal } from "react-dom";
+import EditModal from "./EditModal";
+import "./SmallCard.css";
+
 function AddCardButton() {
+  const [showModal, setShowModal] = useState(false);
+
+  const getTodayDate = () => {
+    const today = new Date();
+    return today.toLocaleDateString();
+  };
+
   return (
-    <>
-    </>
-  )
+    <div className="cards-container">
+      <button onClick={() => setShowModal(true)} className="card-btn">
+        <div className="card-content">
+          <h2 className="card-title">Add Diary Entry</h2>
+          <p className="card-date">{getTodayDate()}</p>
+        </div>
+      </button>
+      {showModal &&
+        createPortal(
+          <EditModal onClose={() => setShowModal(false)} />,
+          document.body
+        )}
+    </div>
+  );
 }
 
 export default AddCardButton;
