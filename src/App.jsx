8b1bf246bd79d4f9bb78fe2f7bd5098f2a8d516d
darynazin <danayu@ukr.net>
2024-12-24 20:55:12 +0100

    feature/ added form validation and styles, fixed logics

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import "./App.css";
 import React, { useState, useEffect } from "react";
 import initializeFakeData from "./fakeData";
 import Container from "./components/Container";
-import EditModal from "./components/EditModal"
+import EditModal from "./components/EditModal";
 
 function App() {
   initializeFakeData();
@@ -12,6 +12,7 @@ function App() {
     JSON.parse(localStorage.getItem("myFakeData")) || []
   );
   const [card, setCard] = useState({});
+  
 
   const handleClose = () => {
     setEditlVisible(false);
@@ -49,7 +50,7 @@ function App() {
 
     localStorage.setItem("myFakeData", JSON.stringify(updatedItems));
     setStoredItems(updatedItems);
-    handleclick();
+    console.log(updatedItems)
   };
 
   useEffect(() => {
@@ -61,14 +62,27 @@ function App() {
     localStorage.setItem("myFakeData", JSON.stringify(storedItems));
   }, [storedItems]);
 
-  console.log(storedItems);
-
   return (
     <div className="bg-[#E8E1D3] h-screen text-[#333] flex flex-col items-center">
-      <header className="pt-24 font-serif text-4xl mb-12" >My Personal Diary</header>
-      <Container storedItems={storedItems} onAdd={handleAdd} onEdit={handleEdit} onClose={handleClose} />
-      {isEditVisible && <EditModal item={card} onClose={handleClose} onSave={handleSave}/>}
-      {isAddVisible && <EditModal onClose={handleClose} onSave={handleSave}/>}
+      <header className="pt-24 font-serif text-4xl mb-12">
+        My Personal Diary
+      </header>
+      <Container
+        storedItems={storedItems}
+        onAdd={handleAdd}
+        onEdit={handleEdit}
+        onClose={handleClose}
+      />
+      {isEditVisible && (
+        <EditModal item={card} onClose={handleClose} onSave={handleSave} storedItems={storedItems}/>
+      )}
+      {isAddVisible && (
+        <EditModal
+          onClose={handleClose}
+          onSave={handleSave}
+          storedItems={storedItems}
+        />
+      )}
     </div>
   );
 }
